ec3fe872ad559115a42e2939e6cbe2f409df5329
Chris Johnson <chris@cjohnson.io>
2013-12-19 18:46:38 -0800

    More configuration details, yay!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     sass: {
-      compile: {
-        files: {
-          'public/css/home.css': 'app/assets/css/home.scss'
-        }
+      dist: {
+        files: [{
+          expand: true,
+          src: ['app/assets/css/*.scss'],
+          dest: 'public/css',
+          flatten: true,
+          ext: '.css'
+        }]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: "app/assets/css/*.scss",
+        tasks: ["sass"]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['sass']);
-}
+};
