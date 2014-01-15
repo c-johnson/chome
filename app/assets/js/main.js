39266fcb2588df2b450bc336a9eeef1241d8af95
Chris Johnson <chris@cjohnson.io>
2014-01-14 21:51:33 -0800

    foundation added

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,15 +1,21 @@
 require.config({
+  shim: {
+    'foundation': {
+      deps: ['jquery'],
+      exports: 'Foundation'
+    }
+  },
   baseUrl: "public/js",
   paths: {
     "angular": "../bowerc/angular/angular",
-    "jquery": "../bowerc/jquery/jquery"
+    "jquery": "../bowerc/jquery/jquery",
+    "foundation": "../bowerc/foundation/js/foundation"
   },
   waitSeconds: 15
 });
 
-require( ["angular", "home"],
+require( ["angular", "home", "foundation"],
 function(home) {
-
+  $(document).foundation();
   console.log('loaded all modules');
-
 });
