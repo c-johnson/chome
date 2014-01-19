e34ffbb1f7a882efd7065712fb0e415e39991f0b
Chris Johnson <chris@cjohnson.io>
2014-01-18 23:01:35 -0800

    Fixed silly top bar issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,5 +49,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.registerTask('default', ['sass', 'copy']);
 };
--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,6 +3,10 @@ require.config({
     'foundation': {
       deps: ['jquery', 'modernizr'],
       exports: 'Foundation'
+    },
+    'fo-topbar': {
+      deps: ['foundation'],
+      exports: 'FO-Topbar'
     }
   },
   baseUrl: "public/js",
@@ -10,12 +14,13 @@ require.config({
     "angular": "../bowerc/angular/angular",
     "jquery": "../bowerc/jquery/jquery",
     "modernizr": "../bowerc/modernizr/modernizr",
-    "foundation": "../bowerc/foundation/js/foundation"
+    "foundation": "../bowerc/foundation/js/foundation",
+    "fo-topbar": "../bowerc/foundation/js/foundation/foundation.topbar"
   },
   waitSeconds: 15
 });
 
-require( ["angular", "home", "foundation"],
+require( ["angular", "home", "foundation", "fo-topbar"],
 function(home) {
   $(document).foundation();
   console.log('loaded all modules');
