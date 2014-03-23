e803db7e56ea5944659d95092471e70b5f89ea61
Chris Johnson <chris@cjohnson.io>
2014-03-23 11:15:31 -0700

    delted fbomb

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,10 @@
-define(function() {
-  var kay="key";
+define(['jquery'], function() {
+  $(document).on('keydown', function (evt) {
+    var root = "http://localhost:9000"
+    if (evt.which == 37) 
+      window.location.assign(root + "/links");
+    else if (evt.which == 39) {
+      window.location.assign(root + "/home");
+    }
+  });
 });
