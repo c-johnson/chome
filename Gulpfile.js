9bf4770257fa4472dbd814e290479767fdbada3f
Chris Johnson <chris@cjohnson.io>
2014-05-22 17:08:58 -0700

    LiveReload simply doesn't work :(

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,13 +6,20 @@ var livereload = require('gulp-livereload');
 var paths = {
   scripts: ['app/assets/js/**/*.js'],
   style: ['app/assets/style/**/*.scss'],
-  images: ['app/assets/img/**/*.scss']
+  fonts: ['app/assets/fonts/**'],
+  images: ['app/assets/img/**']
 };
 
 gulp.task('scripts', function () {
   return gulp.src(paths.scripts)
     .pipe(gulp.dest('public/js'));
 });
+
+// gulp.task('server', function(next) {
+//   var connect = require('connect'),
+//       server = connect();
+//   server.use(connect.static("public")).listen(process.env.PORT || 8000, next);
+// });
 
 gulp.task('style', function () {
   return gulp.src(paths.style)
@@ -29,20 +36,43 @@ gulp.task('images', function () {
     .pipe(gulp.dest('public/img'));
 });
 
+gulp.task('fonts', function () {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest('public/fonts'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.style, ['style']);
   gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.fonts, ['fonts']);
+
   var server = livereload();
-  gulp.watch('public/js/**.js').on('change', function(file) {
-      server.changed(file.path);
+  gulp.watch('public/js/**/*.js').on('change', function(file) {
+    server.changed(file.path);
   });
-  gulp.watch('public/style/**.css').on('change', function(file) {
-      server.changed(file.path);
+  gulp.watch('public/css/**/*.css').on('change', function(file) {
+    console.log('scss is being compileedd1!!!!');
+
+    // The gulp.watch api is kind of silly here.  It gives you an absolute path for a css file on your system that has changed.  However, the livereload server wants to know the path the web server will serve the file at.  
+
+    // file.path looks like:
+    // /Users/chris/ciqss/gowrk/src/github.com/c-johnson/chome/public/css/main/home.css
+    // |_____________________________________________________________|________________|
+    //                            |                                           |
+    //              Preceding file path, discard                        web server 
+    //                9 tokens (+ leading slash)
+
+    var webPath = file.path.split("/").slice(10).join("/");
+    console.log(webPath);
+    server.changed(webPath);
   });
   gulp.watch('public/img/**').on('change', function(file) {
-      server.changed(file.path);
+    server.changed(file.path);
+  });
+  gulp.watch('public/fonts/**').on('change', function(file) {
+    server.changed(file.path);
   });
 });
 
-gulp.task('default', ['scripts', 'style', 'images']);
+gulp.task('default', ['scripts', 'style', 'images', 'fonts']);
