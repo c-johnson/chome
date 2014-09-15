99fb2de71714915f0c80e0f4972c621667d90e0a
Chris Johnson <chris@cjohnson.io>
2014-09-15 19:37:48 +0800

    Added blog titles and such

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -19,35 +19,42 @@ type Resp struct {
 
 func (c App) Home() revel.Result {
 	active := "home"
+	c.RenderArgs["title"] = "World of Chris"
 	return c.Render(active)
 }
 
 func (c App) Application() revel.Result {
 	active := "app"
+	c.RenderArgs["title"] = "Application"
 	return c.Render(active)
 }
 
 func (c App) Contact() revel.Result {
 	active := "contact"
+	c.RenderArgs["title"] = "Contact"
 	return c.Render(active)
 }
 
 func (c App) Work() revel.Result {
 	active := "work"
+	c.RenderArgs["title"] = "Work"
 	return c.Render(active)
 }
 
 func (c App) Links() revel.Result {
 	active := "links"
+	c.RenderArgs["title"] = "Links"
 	return c.Render(active)
 }
 
 func (c App) Wep() revel.Result {
+	c.RenderArgs["title"] = "Wep"
 	return c.Redirect("https://rawgithub.com/c-johnson/wep/master/index.html")
 }
 
 func (c App) Test() revel.Result {
 	active := "test"
+	c.RenderArgs["title"] = "Test"
 	return c.Render(active)
 }
 
@@ -62,6 +69,11 @@ func (c App) Words(post string) revel.Result {
 				htmlString := string(Html(post).ReturnHtml())
 				c.RenderArgs["content"] = htmlString
 				c.RenderArgs["active"] = "words"
+				if publicPost.Shortname == "momentum" {
+					c.RenderArgs["title"] = "Why I can't even order a bowl of goddamn noodles."
+				} else {
+					c.RenderArgs["title"] = "A blog post!"
+				}
 				return c.RenderTemplate("App/word.html")
 			}
 		}
